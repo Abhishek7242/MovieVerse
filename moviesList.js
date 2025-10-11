abe7c42abc123f0bf5d0cb30106545910a54afe8
Abhishek Verma <vabhishak45@gmail.com>
2025-10-11 15:11:41 +0530

    updated

--- moviesList.js
+++ moviesList.js
@@ -5,6 +5,7 @@ const movies = [
     { name: "Interstellar", url: "https://image.tmdb.org/t/p/w780/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg", rating: 8.6 },
     { name: "Parasite", url: "https://image.tmdb.org/t/p/w780/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg", rating: 8.6 },
     { name: "The Matrix", url: "https://image.tmdb.org/t/p/w780/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg", rating: 8.7 },
+    { name: "The Gray Man", url: "https://assets-prd.ignimgs.com/2022/06/07/the-gray-man-button-1654637490629.jpg", rating: 8.7 },
     { name: "Dune", url: "https://media.vanityfair.com/photos/5e962efaac720b00089fd0a9/1:1/w_1200,h_1200,c_limit/0520-Dune-Tout-Lede-a.jpg", rating: 8.5 },
     { name: "Dune part 2", url: "https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Dune_Part_Two_poster.jpeg/250px-Dune_Part_Two_poster.jpeg", rating: 8.9 },
 ];
